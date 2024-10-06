dae4775b36389f3fabcb03fb8c5baf5def15e841
Tarek Sallam <tarek@sallam.ca>
2024-10-06 12:08:49 -0400

    added more to do list

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import { updatePov, onKeyDown, onKeyUp } from './userControls';
 import { renderer, planets, scene, camera,} from './createScene';
 
-// TODO: move up and down
-// TODO: create space boundary
-// TODO: can't go through planets
-// TODO: saturn rings (planet rings)
-// TODO: restructure code into different files
-// TODO: move faster with holding comand
+// TODO:
+// implement controls to move up and down ✓
+// create space boundary so user can't go too far out
+// make it so that user can't go through planets
+// make saturn's rings look good
+// implement controls for user to move faster if desired
+// translit orbit data into actual orbits
+// make smooth animations for the orbits
+// make orbit controls not mess up the keyboard controls ✓
+
 let orbits;
 
 const getOrbits = async () => {
